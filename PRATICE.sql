8c1eaed3723491ee8a65b30a45a93885fad41bca
Nikhil Patil <nikhil@example.com>
2025-10-14 15:43:11 +0530

    updated emp salary

--- PRATICE.sql
+++ PRATICE.sql
@@ -5,3 +5,11 @@ SELECT * FROM departments;
 SELECT * FROM designations;
 
 SELECT * FROM emp;
+
+UPDATE emp
+SET salary = 50000
+WHERE ï»¿employee_id BETWEEN "EMP1" AND "EMP35";
+
+SET SQL_SAFE_UPDATES = 0;
+
+
